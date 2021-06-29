eb9211c0a61fa125070915a0818e98d14028eb07
cirodelia74 <85739966+cirodelia74@users.noreply.github.com>
2021-06-29 18:20:56 +0200

    Implementatto TestAzzardo_Moneta.java

--- TestAzzardo_Dado.java
+++ TestAzzardo_Dado.java
@@ -4,14 +4,44 @@ public class TestAzzardo_Dado {
 	public static void main(String[] args) {
 		
 		int result;
+
+		int cont1 = 0;
+		int cont2 = 0;
+		int cont3 = 0;
+		int cont4 = 0;
+		int cont5 = 0;
+		int cont6 = 0;
 		
 		Dado1 d1 = new Dado1();
 		Dado2 d2 = new Dado2();
 		Dado3 d3 = new Dado3();
 		
 		// TODO: implementare un test per scoprire quale dei tre dadi è truccato.
-		
 
+		for (int i=0; i<1000; i++) {
+
+			result = d3.lancio();
+			
+			if (result == 1)
+				cont1++;
+			if (result == 2)
+				cont2++;
+			if (result == 3)
+				cont3++;
+			if (result == 4)
+				cont4++;
+			if (result == 5)
+				cont5++;
+			if (result == 6)
+				cont6++;
+		}
+
+		System.out.println("Faccia 1 su 1000 lanci: " +cont1);
+		System.out.println("Faccia 2 su 1000 lanci: " +cont2);
+		System.out.println("Faccia 3 su 1000 lanci: " +cont3);
+		System.out.println("Faccia 4 su 1000 lanci: " +cont4);
+		System.out.println("Faccia 5 su 1000 lanci: " +cont5);
+		System.out.println("Faccia 6 su 1000 lanci: " +cont6);
 	}
 
 }
--- TestAzzardo_Moneta.java
+++ TestAzzardo_Moneta.java
@@ -4,12 +4,62 @@ public class TestAzzardo_Moneta {
 	public static void main(String[] args) {
 		
 		int result;
+		final int NUM_LANCI = 1000;
+
+		// contatori per i risultati TESTA e CROCE
+		// TESTA se lancio() ritorna 0
+		// CROCE se lancio() ritorna 1
+		int contTesta = 0;
+		int contCroce = 0;
+
+		// variabili che conterrano la percentuale di uscita delle 2 opzioni
+		double percentualeTesta;
+		double percentualeCroce;
 		
 		Moneta1 m1 = new Moneta1();
 		Moneta2 m2 = new Moneta2();
 		
-		// TODO: implementare un test per scoprire quale delle due monete è truccata
-		
+		// Testo la moneta m1	
+		for (int i=0; i<NUM_LANCI; i++) {
+
+			result = m1.lancio();
+			
+			if (result == 0)
+				contTesta++;
+			if (result == 1)
+				contCroce++;
+		}
+
+		// mostro le percentuali con al massimo 2 cifre decimali
+		percentualeTesta = Math.round(((contTesta * 100.0) / NUM_LANCI) * 100.0) / 100.0;
+		percentualeCroce = Math.round(((contCroce * 100.0) / NUM_LANCI) * 100.0) / 100.0;
+		System.out.println("\nRisultati del lancio della moneta 1");
+		System.out.println("===================================");
+		System.out.println("Testa: " +contTesta +" su " +NUM_LANCI + " lanci. (" +percentualeTesta +"%)");
+		System.out.println("Croce: " +contCroce +" su " +NUM_LANCI + " lanci. (" +percentualeCroce +"%)");
+	    
+
+		// Testo la moneta m2
+
+		contTesta = 0;
+		contCroce = 0;
+
+		for (int i=0; i<NUM_LANCI; i++) {
+
+			result = m2.lancio();
+			
+			if (result == 0)
+				contTesta++;
+			if (result == 1)
+				contCroce++;
+		}
+		// mostro le percentuali con al massimo 2 cifre decimali
+		percentualeTesta = Math.round(((contTesta * 100.0) / NUM_LANCI) * 100.0) / 100.0;
+		percentualeCroce = Math.round(((contCroce * 100.0) / NUM_LANCI) * 100.0) / 100.0;
+		System.out.println("\nRisultati del lancio della moneta 2");
+		System.out.println("===================================");
+		System.out.println("Testa: " +contTesta +" su " +NUM_LANCI + " lanci. (" +percentualeTesta +"%)");
+		System.out.println("Croce: " +contCroce +" su " +NUM_LANCI + " lanci. (" +percentualeCroce +"%)");
 	}
 
 }
