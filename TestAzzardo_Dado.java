d422ae27784e8eadd652447691ee9d50c05bf453
djack1010 <giachi.iada@gmail.com>
2021-07-06 14:50:10 +0200

    caricamento soluzione dado

--- TestAzzardo_Dado.java
+++ TestAzzardo_Dado.java
@@ -9,7 +9,20 @@ public class TestAzzardo_Dado {
 		Dado2 d2 = new Dado2();
 		Dado3 d3 = new Dado3();
 		
-		// TODO: implementare un test per scoprire quale dei tre dadi è truccato.
+		float counter1 = 0;
+		float counter2 = 0;
+		float counter3 = 0;
+		int iterazioni = 999999;
+		for (int i = 0; i < iterazioni; i++){
+		  System.out.println(i);
+		  counter1 += d1.lancio();
+		  counter2 += d2.lancio();
+		  counter3 += d3.lancio();
+		}
+		
+		System.out.println("Media Dado1: " + counter1 / iterazioni);
+		System.out.println("Media Dado2: " +counter2 / iterazioni);
+		System.out.println("Media Dado3: " +counter3 / iterazioni);
 		
 
 	}
