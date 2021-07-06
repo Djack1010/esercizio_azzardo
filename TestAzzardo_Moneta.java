4f03ad9b514085eb5a174db3e5c0d33c428d98a4
djack1010 <giachi.iada@gmail.com>
2021-07-06 14:42:04 +0200

    caricamento soluzione moneta

--- TestAzzardo_Moneta.java
+++ TestAzzardo_Moneta.java
@@ -10,6 +10,23 @@ public class TestAzzardo_Moneta {
 		
 		// TODO: implementare un test per scoprire quale delle due monete è truccata
 		
+		float counter_m1 = 0;
+		float counter_m2 = 0;
+		int numero_lanci = 999999;
+
+		for(int i=0; i<numero_lanci;i++){
+
+			counter_m1 += m1.lancio();
+			counter_m2 += m2.lancio();
+		
+		}
+
+		float media_m1 = counter_m1 / numero_lanci;
+		float media_m2 = counter_m2 / numero_lanci;
+		
+		System.out.println("Media moneta 1: " + media_m1);
+		System.out.println("Media moneta 2: " + media_m2);
+
 	}
 
 }
